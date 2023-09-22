6df64cebb5031d937c2e4425763c0e004b941268
Marcin Wysocki <marcin.wysocki@surferseo.com>
2023-09-22 11:03:10 +0200

    feat: decrease height, add collapse option

--- apps/surfer/src/locations/Sidebar.tsx
+++ apps/surfer/src/locations/Sidebar.tsx
@@ -1,13 +1,17 @@
 import { EntrySys, SidebarAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SurferContainer } from '../components/SurferContainer';
 import { useFieldSelection } from '../hooks/useFieldSelection';
 import { useSurfer } from '../hooks/useSurfer';
 import { SurferContext, SurferRpcCommands, SurferRpcMessage } from '../types';
 import { useContentHtml } from '../hooks/useContentHtml';
 import { useConfigurationDialog } from '../hooks/useConfigurationDialog';
-import { Flex, Note } from '@contentful/f36-components';
+import { Note } from '@contentful/f36-components';
+
+const FULL_SIZE_PX = 550;
+const WARNING_SIZE_PX = 70;
+const EXPAND_BUTTON_SIZE_PX = 40;
 
 const buildShareToken = ({ id, space }: EntrySys) => `${space.sys.id}_${id}`;
 
@@ -16,8 +20,11 @@ const Sidebar = () => {
   const { entry, window } = useSDK<SidebarAppSDK>();
   const [selectedFields, FieldSelection, richTextFields] = useFieldSelection(Object.values(entry.fields));
   const contentHtml = useContentHtml(richTextFields, selectedFields);
+  const widgetSizePx = richTextFields.length ? FULL_SIZE_PX : WARNING_SIZE_PX;
   const shareToken = buildShareToken(entry.getSys());
   const { openConfigurationDialog, isConfigurationOpen } = useConfigurationDialog(shareToken);
+
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const onReady = ({ setHtml, configureView }: SurferContext) => {
     setHtml(contentHtml);
@@ -36,21 +43,22 @@ const Sidebar = () => {
     shareToken,
     onReady,
     onRpcMessage,
-  })
+  });
 
   useEffect(() => {
-    window.updateHeight(richTextFields.length ? 700 : 70);
-  }, [window, richTextFields]);
+    window.updateHeight(widgetSizePx);
+  }, [window, richTextFields, widgetSizePx]);
 
   useEffect(() => {
     setHtml?.(contentHtml);
   }, [contentHtml, setHtml]);
 
+  useEffect(() => {
+    window.updateHeight(isExpanded ? widgetSizePx : EXPAND_BUTTON_SIZE_PX);
+  }, [isExpanded, widgetSizePx, window]);
+
   return richTextFields.length ? (
-    <Flex flexDirection="column" justifyContent="space-between" fullHeight>
-      <FieldSelection />
-      <SurferContainer ref={iframeContainerRef} isLoading={isLoading || isConfigurationOpen} flex="2 0" />
-    </Flex>
+    <SurferContainer ref={iframeContainerRef} isLoading={isLoading || isConfigurationOpen} flex="2 0" isExpanded={isExpanded} toggleExpanded={setIsExpanded} />
   ) : (
     <Note variant="warning">Add a RichText field to enable Surfer!</Note>
   );
